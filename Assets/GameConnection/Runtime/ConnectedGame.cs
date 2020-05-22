18adb0cffa0bff2da7071e238dd92c75702de05f
Jonathan Levin <jon.levin25@gmail.com>
2020-05-22 16:38:28 +0300

    Merge Editor_ConnectedGame + Runtime_ConnectedGame

--- Assets/GameConnection/Runtime/ConnectedGame.cs
+++ Assets/GameConnection/Runtime/ConnectedGame.cs
@@ -9,38 +9,44 @@ using UnityEditor;
 namespace GameConnection
 {
     [Serializable]
-    public class Runtime_ConnectedGame
+    public class ConnectedGame
     {
         [ReadOnly, SerializeField] private string inputPayloadTypeName;
         [ReadOnly, SerializeField] private string outputPayloadTypeName;
-        [ReadOnly, SerializeField] private int sceneBuildIdx;
+        [ReadOnly, SerializeField] private string scenePath;
+        // [ReadOnly, SerializeField] private int sceneBuildIdx;
 
         // Types Not in use currently - but good to have this info
         // as we might want to connect games via payload type (dynamically) in the future
         public string InputPayloadTypeName => inputPayloadTypeName;
         public string OutputPayloadTypeName => outputPayloadTypeName;
-        public int SceneBuildIdx => sceneBuildIdx;
+        public string ScenePath => scenePath;
+        
+        // public int SceneBuildIdx => sceneBuildIdx;
 
 #if UNITY_EDITOR 
         public void CopyTo(SerializedProperty prop)
         {
             var inputTypeNameProp = prop.FindPropertyRelative(nameof(inputPayloadTypeName));
             var outputTypeNameProp = prop.FindPropertyRelative(nameof(outputPayloadTypeName));
-            var sceneIdxTypeNameProp = prop.FindPropertyRelative(nameof(sceneBuildIdx));
+            var scenePathProp = prop.FindPropertyRelative(nameof(scenePath));
+            // var sceneIdxTypeNameProp = prop.FindPropertyRelative(nameof(sceneBuildIdx));
 
             inputTypeNameProp.stringValue = inputPayloadTypeName;
             outputTypeNameProp.stringValue = outputPayloadTypeName;
-            sceneIdxTypeNameProp.intValue = sceneBuildIdx;
+            scenePathProp.stringValue = scenePath;
+            // sceneIdxTypeNameProp.intValue = sceneBuildIdx;
         }
 #endif
         
-        public Runtime_ConnectedGame(Type inputPayloadTypeName, Type outputPayloadTypeName, int sceneBuildIdx)
+        public ConnectedGame(Type inputPayloadTypeName, Type outputPayloadTypeName, string scenePath)
         {
+            if (scenePath ==null) Debug.LogError("ScenePath cannot be null!");
             if (!Helper.ValidatePayloadTypes(inputPayloadTypeName, outputPayloadTypeName)) return;
             
             this.inputPayloadTypeName = inputPayloadTypeName.FullName;
             this.outputPayloadTypeName = outputPayloadTypeName.FullName;
-            this.sceneBuildIdx = sceneBuildIdx;
+            this.scenePath = scenePath;
         }
     }
 }
