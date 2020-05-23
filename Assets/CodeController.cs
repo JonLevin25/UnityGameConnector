43346d11432b12767d55ddd2d71015b390eb93b2
Jonathan Levin <jon.levin25@gmail.com>
2020-05-23 03:26:29 +0300

    SafeGame - controllers

---
+++ Assets/CodeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class CodeController : MonoBehaviour
+{
+    [SerializeField] private GameObject correctCodeScreen;
+    [SerializeField] private GameObject wrongCodeScreen;
+    [SerializeField] private float feedbackTime = 0.8f;
+    
+    
+    private string _correctCode;
+    private Action _onComplete;
+    
+    public void Init(string correctCode, Action onComplete)
+    {
+        _correctCode = correctCode;
+        _onComplete = onComplete;
+        HideScreens();
+    }
+
+    // Called from scene
+    public void OnCodeAttempt(string code)
+    {
+        if (IsCorrect(code)) OnCodeCorrect();
+        else OnCodeIncorrect(code);
+    }
+
+    private void OnCodeCorrect()
+    {
+        Debug.Log($"Code correct! ({_correctCode})");
+        ShowCodeScreen(correctCodeScreen);
+        StartCoroutine(DelayCallback(feedbackTime, _onComplete));
+    }
+
+    private void OnCodeIncorrect(string entered)
+    {
+        Debug.Log($"Code wrong! (Entered: {entered}. Expected: {_correctCode})");
+        ShowCodeScreen(wrongCodeScreen);
+        StartCoroutine(DelayCallback(feedbackTime, HideScreens));
+    }
+
+    private void ShowCodeScreen(GameObject codeScreen)
+    {
+        HideScreens();
+        codeScreen.SetActive(true);
+    }
+
+    private void HideScreens()
+    {
+        correctCodeScreen.SetActive(false);
+        wrongCodeScreen.SetActive(false);
+    }
+
+    private IEnumerator DelayCallback(float seconds, Action callback)
+    {
+        yield return new WaitForSeconds(seconds);
+        callback?.Invoke();
+    }
+    
+    private bool IsCorrect(string code) => code == _correctCode;
+}
